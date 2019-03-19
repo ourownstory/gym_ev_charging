97cda0887a61b56140cddbb9978d9b830ead47c4
Oskar Triebe <oskar.triebe@merantix.com>
2019-03-19 13:29:31 -0700

    no clue

--- gym_ev_charging/config_gym.py
+++ gym_ev_charging/config_gym.py
@@ -14,12 +14,12 @@ class config_default:
         self.EPS_LEN = 4*24*7
         self.EVAL_EPS_LEN = 4*24*7
 
-        self.NUM_STATIONS = 3
-        self.TRANSFORMER_LIMIT = 0.33  # [0, 1]
+        self.NUM_STATIONS = 1
+        self.TRANSFORMER_LIMIT = 1  # [0, 1]
         self.solar_behind_meter = 0  # [0, (1 - TRANSFORMER_LIMIT) / TRANSFORMER_LIMIT]
         self.charge_empty_factor = 0  # [0, 2]
 
-        self.REWARD_WEIGHTS = (1, 0, 0)
+        self.REWARD_WEIGHTS = (1, 1, 0)
         self.penalize_unecessary_actions = 100
 
         self.data_file = "sessions_161718_95014_top10.csv"
